35c5da61d9f44f82d855749de5310e249d769374
khaledkhamis <khaledkhamis@live.com>
2017-02-11 22:01:40 +0200

    Added getLeafs function

--- src/Gzero/EloquentTree/Model/Tree.php
+++ src/Gzero/EloquentTree/Model/Tree.php
@@ -343,6 +343,17 @@ abstract class Tree extends \Eloquent {
         return static::whereNull(static::getTreeColumn('parent'));
     }
 
+ /**
+     * Gets all leaf nodes
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public static function getLeafs()
+    {
+        $parents  = static::select('parent_id')->whereNotNull('parent_id')->distinct()->get()->pluck('parent_id')->all();
+        return static::wherenotin('id',$parents);
+    }
+
     /**
      * @param null $name
      *
